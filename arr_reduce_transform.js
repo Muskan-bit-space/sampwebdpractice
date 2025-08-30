f2d067fb82d5cca9e2fb23af6edf5b45a9857107
Muskan-bit-space <muskan4mann@gmail.com>
2025-08-30 13:54:31 +0530

    Create arr_reduce_transform.js

---
+++ arr_reduce_transform.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/array-reduce-transformation/submissions/1753267701/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val=init;
+    for(let i=0;i<nums.length;i++){
+
+        val=fn(val,nums[i]);
+        console.log(val)
+        // init=val;
+    }
+
+    return val;
+    // return fn(init,nums[i++]);
+};
